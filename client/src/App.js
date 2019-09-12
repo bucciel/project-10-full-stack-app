53d14032f2a821f41e02106317f95fdc55decb15
bucciel <51244122+bucciel@users.noreply.github.com>
2019-09-12 10:54:13 -0400

    add components to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,19 @@
-import React from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Courses from './components/Courses';
+import CourseDetail from './components/CourseDetail';
+import CreateCourse from './components/CreateCourse';
+import UpdateCourse from './components/UpdateCourse';
 
 
+import UserSignUp from './components/UserSignUp';
+import UserSignIn from './components/UserSignIn';
+import UserSignOut from './components/UserSignOut';
 
-class App extends React.Component {     //Renders application components and routes
 
-  state = {           //State declarations for app components
-    courses: [],
-    isLoading: true,
-  }
+class App extends Component {     //Renders application components and routes
 
-  componentDidMount() {
-    fetch(`http://localhost:5000/api/courses`)
-      .then(response => response.json())
-      .then(courses => this.setState({
-        courses: courses,
-        isLoading: false
-      }))
-      .then(() => console.log(this.state.courses))
-      .catch(error => console.log("Sorry, there's nothing here"));
-  }
-
-  render() {
-    return this.state.isLoading ? (<h2>Loading...</h2>) : (
-      <div className="container">
-        {
-          this.state.courses.map(course => {
-            return (
-              <ul key={course.id}>
-                <li>{course.id}</li>
-                <li>{course.title}</li>
-                <li>{course.description}</li>
-                <li>{course.estimatedTime}</li>
-                <li>{course.materialsNeeded}</li>
-              </ul>
-            );
-          })
-        }
-      </div>
-    )
-  }
 }
 
 export default App;
