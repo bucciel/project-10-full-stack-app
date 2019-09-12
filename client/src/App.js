bb04c06f1303dfcdc548b3aa68c27a4c810c9011
bucciel <51244122+bucciel@users.noreply.github.com>
2019-09-12 09:55:41 -0400

    render courses list test

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {     //Renders application components and routes
+
+  state = {           //State declarations for app components
+    courses: [],
+    isLoading: true,
+  }
+
+  componentDidMount() {
+    fetch(`http://localhost:5000/api/courses`)
+      .then(response => response.json())
+      .then(courses => this.setState({
+        courses: courses,
+        isLoading: false
+      }))
+      .then(() => console.log(this.state.courses))
+      .catch(error => console.log("Sorry, there's nothing here"));
+  }
+
+  render() {
+    return this.state.isLoading ? (<h2>Loading...</h2>) : (
+      <div className="container">
+        {
+          this.state.courses.map(course => {
+            return (
+              <ul key={course.id}>
+                <li>{course.id}</li>
+                <li>{course.title}</li>
+                <li>{course.description}</li>
+                <li>{course.estimatedTime}</li>
+                <li>{course.materialsNeeded}</li>
+              </ul>
+            );
+          })
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ client/src/components/NotFound.js
@@ -0,0 +1,18 @@
+/*
+//React import statement
+import React, { Component } from 'react';
+
+class NotFound extends Component {      //Renders 404 error photo to user when requested course can't be found
+    render() {
+        return (
+            <div className="Not Found">
+                <li>
+                    <img src={require('./errorImages/error4.png')} alt={'404 page not found'} className={'center'} />
+                </li>
+            </div>
+        );
+    }
+}
+
+export default NotFound;
+*/
