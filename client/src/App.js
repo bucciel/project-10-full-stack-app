909c17fe124602b7dbe9eb9528d6ff779c0f7f36
bucciel <51244122+bucciel@users.noreply.github.com>
2019-09-16 12:28:18 -0400

    userSignUp link works

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,10 @@ import './styles/global.css';
 
 /* import routes for components */
 import Header from './components/Header';
-// import UserSignUp from './components/UserSignUp';
+import Authenticated from './components/Authenticated';
+import UserSignUp from './components/UserSignUp';
 import UserSignIn from './components/UserSignIn';
 // import UserSignOut from './components/UserSignOut';
-import Authenticated from './components/Authenticated';
 // import Courses from './components/Courses';
 // import CourseDetail from './components/CourseDetail';
 // import CreateCourse from './components/CreateCourse';
@@ -23,7 +23,7 @@ import PrivateRoute from './PrivateRoute';
 /* connect components to context */
 const HeaderWithContext = withContext(Header);
 const AuthWithContext = withContext(Authenticated);
-// const UserSignUpWithContext = withContext(UserSignUp);
+const UserSignUpWithContext = withContext(UserSignUp);
 const UserSignInWithContext = withContext(UserSignIn);
 // const UserSignOutWithContext = withContext(UserSignOut);
 // const CourseDetailWithContext = withContext(CourseDetail);
@@ -38,7 +38,7 @@ class App extends Component {
       <Router>
         <div>
           <Route render={({ location }) => <HeaderWithContext location={location.pathname} />} />
-          {/* <HeaderWithContext /> */}
+
           <Switch>
             <PrivateRoute path="/authenticated" component={AuthWithContext} />
             {/*   
@@ -47,8 +47,8 @@ class App extends Component {
             <PrivateRoute exact path="/courses/:id/update" component={UpdateCourseWithContext} />
             <Route exact path="/courses/:id" component={CourseDetailWithContext} /> */}
             <Route path="/signin" component={UserSignInWithContext} />
-            {/* 
             <Route path="/signup" component={UserSignUpWithContext} />
+            {/*
             <Route path="/signout" component={UserSignOutWithContext} /> */}
           </Switch>
         </div>
