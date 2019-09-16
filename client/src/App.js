f01d7a5a0f286e7786699dde30f10d4c2b570153
bucciel <51244122+bucciel@users.noreply.github.com>
2019-09-16 15:56:08 -0400

    add render routes

--- client/src/App.js
+++ client/src/App.js
@@ -39,18 +39,18 @@ class App extends Component {
       <Router>
         <div>
           <Route render={({ location }) => <HeaderWithContext location={location.pathname} />} />
-
           <Switch>
             <Route exact path="/" component={CoursesWithContext} />
-            <PrivateRoute path="/authenticated" component={AuthWithContext} />
-            {/* 
-            <PrivateRoute path="/courses/create" component={CreateCourseWithContext} />
-            <PrivateRoute exact path="/courses/:id/update" component={UpdateCourseWithContext} /> */}
             <Route exact path="/courses/:id" component={CourseDetailWithContext} />
-            {/* <Route path="/signin" component={UserSignInWithContext} />
-            <Route path="/signup" component={UserSignUpWithContext} /> */}
-            {/*
-            <Route path="/signout" component={UserSignOutWithContext} /> */}
+            <PrivateRoute path="/authenticated" component={AuthWithContext} />
+            {/* <PrivateRoute path="/courses/create" component={CreateCourseWithContext} /> */}
+            {/* <PrivateRoute exact path="/courses/:id/update" component={UpdateCourseWithContext} /> */}
+            <Route path="/signin" component={UserSignInWithContext} />
+            <Route path="/signup" component={UserSignUpWithContext} />
+            {/* <Route path="/signout" component={UserSignOutWithContext} /> */}
+            {/*<Route path="/forbidden" component={Forbidden} />*/}
+            {/*<Route path="/error" component={UnhandledError} />*/}
+            {/*<Route component={NotFound} />*/}
           </Switch>
         </div>
       </Router>
