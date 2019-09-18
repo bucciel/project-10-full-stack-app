63da8d6e47e1569b56489ab725672f5c6db0db8e
bucciel <51244122+bucciel@users.noreply.github.com>
2019-09-18 08:56:03 -0400

    createCourse button works

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,13 @@ import './styles/global.css';
 
 /* import routes for components */
 import Header from './components/Header';
+import Courses from './components/Courses';
+import CourseDetail from './components/CourseDetail';
 import UserSignUp from './components/UserSignUp';
 import UserSignIn from './components/UserSignIn';
 import UserSignOut from './components/UserSignOut';
-import Courses from './components/Courses';
-import CourseDetail from './components/CourseDetail';
 import CreateCourse from './components/CreateCourse';
 import UpdateCourse from './components/UpdateCourse';
-
 // import Error from './components/Error';
 // import Forbidden from './components/Forbidden';
 import NotFound from './components/NotFound';
@@ -21,11 +20,11 @@ import PrivateRoute from './PrivateRoute';
 
 /* connect components to context */
 const HeaderWithContext = withContext(Header);
+const CoursesWithContext = withContext(Courses);
+const CourseDetailWithContext = withContext(CourseDetail);
 const UserSignUpWithContext = withContext(UserSignUp);
 const UserSignInWithContext = withContext(UserSignIn);
 const UserSignOutWithContext = withContext(UserSignOut);
-const CoursesWithContext = withContext(Courses);
-const CourseDetailWithContext = withContext(CourseDetail);
 const CreateCourseWithContext = withContext(CreateCourse);
 const UpdateCourseWithContext = withContext(UpdateCourse);
 
@@ -39,9 +38,9 @@ class App extends Component {
           <Route render={({ location }) => <HeaderWithContext location={location.pathname} />} />
           <Switch>
             <Route exact path="/" component={CoursesWithContext} />
-            <Route exact path="/courses/:id" component={CourseDetailWithContext} />
             <PrivateRoute path="/courses/create" component={CreateCourseWithContext} />
             <PrivateRoute exact path="/courses/:id/update" component={UpdateCourseWithContext} />
+            <Route exact path="/courses/:id" component={CourseDetailWithContext} />
             <Route path="/signin" component={UserSignInWithContext} />
             <Route path="/signup" component={UserSignUpWithContext} />
             <Route path="/signout" component={UserSignOutWithContext} />
